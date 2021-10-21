634d0a100e387f21b9aaf0ff5d8901e8b599eb18
cchileshe <chi06001@byui.edu>
2021-10-21 19:18:38 +0200

    updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
+// ========================
+//      MODELS
+//=========================
+
 let taskList = [];
 
-class Task {
+class Task{
     id= new Date().getTime();
     title = '';
     category = 0;
@@ -40,6 +44,11 @@ let categoryList = [
       }
   ]
 
+// ========================
+//    CONTROLLERS
+//=========================
+
+//   add task
 function addTask() {
    
     const task = new Task();
@@ -55,27 +64,87 @@ function addTask() {
 
 }
 
+//delete task
+function deleteTask(event) {
+    let target = event.target;
+    let id = target.id.split('-')[1];
+    let index = taskList.findIndex(item => item.id == id);
+    taskList.splice(index, 1);
+    target.closest('ul').closest('li').remove();
+    updateStorage()
+    if(taskList.length < 1) {
+        window.location.href = 'views/add-task.html';
+    }
+}
 
+function editTask() {
+    console.log('edit task');
+}
+
+// update task
 function updateTaskList(task){
     if (localStorage.getItem('taskList')) {
         getTasksFromStorage();
     }
     taskList.push(task);
     localStorage.setItem('taskList', JSON.stringify(taskList));
+}
 
-console.log(taskList);
+function updateStorage(){
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+}
+
+function redirectToAddTask(){
+    
 }
 
 function getTasksFromStorage() {
     taskList = JSON.parse(localStorage.getItem('taskList'));
+    console.log(taskList);
 }
 
+
+
+let visible = false;
+// sub nav controller
+function showMenu(e) {
+    let el = document.querySelector('.more');
+    let btn = el.querySelector('.more-btn');
+    let menu = el.querySelector('.more-menu');
+    
+    //e.preventDefault();
+    if (!visible) {
+        visible = true;
+        el.classList.add('show-more-menu');
+        menu.setAttribute('aria-hidden', false);
+        // document.addEventListener('mousedown', hideMenu, false);
+    }
+}
+
+function hideMenu() {
+    let el = document.querySelector('.more');
+    let btn = el.querySelector('.more-btn');
+    let menu = el.querySelector('.more-menu');
+    if (visible) {
+        visible = false;
+        el.classList.remove('show-more-menu');
+        menu.setAttribute('aria-hidden', true);
+        document.removeEventListener('mousedown', hideMenu);
+    }
+}
+
+
+
+// ========================
+//      VIEWS
+//=========================
 function viewTasks(){
     const taskView = document.getElementById('taskView');
+
     getTasksFromStorage();
     categoryList.forEach(category =>{
         let singleTask = document.createElement('div');
-        singleTask.className = category.color;
+        singleTask.classList.add(category.color, 'categoryList');
 
         let header = document.createElement('h2');
         
@@ -91,36 +160,93 @@ function viewTasks(){
         items.forEach(item => {
             let liElement = document.createElement('li');
             liElement.className = 'checkbox';
+            //create check box
+            let checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.className = category.color+'-checkmark';
 
 
             let title = document.createElement('span');
             title.innerHTML = item.title;
 
-            //side navigation edit and delete
-            let taskActions = document.createElement('select');
-            taskActions.id = "task";
+            //sub navigation edit and delete
+            let taskActions = document.createElement('div');
+            taskActions.className='more';
+            taskActions.addEventListener('click', showMenu, false)
 
-            let option = document.createElement("option");
-            option.value = 'edit';
-            option.text = 'edit';
-            taskActions.appendChild(option);
-            option = document.createElement("option");
-            option.value = 'delete';
-            option.text = 'delete';
-            taskActions.appendChild(option);
+            let moreBtn = document.createElement("button");
+                moreBtn.id = 'more-btn';
+                moreBtn.className = 'more-btn';
+            taskActions.appendChild(moreBtn);
 
-            //create check box
-            taskActions.appendChild(option);
-            let checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.className = category.color+'-checkmark';
+            let moreBtnImg = document.createElement("img");
+            moreBtnImg.id = 'moreimg';
+            moreBtnImg.className = 'moreimg';
+            moreBtnImg.src ='/public/assets/menu.png';
+                moreBtn.appendChild(moreBtnImg);
+
             
-    
+
+            let moreMenu = document.createElement('div');
+                moreMenu.className = 'more-menu';
+
+             taskActions.appendChild(moreMenu);
+
+            let moreMenuCaret = document.createElement('div');
+            moreMenuCaret.className = 'more-menu-caret';
+            moreMenu.appendChild(moreMenuCaret);
+
+            let moreMenuCaretOuter = document.createElement('div');
+            moreMenuCaretOuter.className = 'more-menu-caret-outer';
+            moreMenuCaret.appendChild(moreMenuCaretOuter);
+
+            let moreMenuCaretInner = document.createElement('div');
+            moreMenuCaretInner.className = 'more-menu-caret-inner';
+            moreMenuCaret.appendChild(moreMenuCaretInner);
+
+            let moreMenuItems = document.createElement('ul');
+            moreMenuItems.className = 'more-menu-items';
+            moreMenuItems.tabindex = '-1';
+            moreMenuItems.ariaHidden = 'true';
+            moreMenuItems.ariaLabelledby = 'more-btn';
+            moreMenuItems.role = 'more-menu-items';
+            moreMenu.appendChild(moreMenuItems);
+
+            let moreMenuItem = document.createElement('li');
+            moreMenuItem.className = 'more-menu-item';
+            moreMenuItem.role = 'presentation';
+            moreMenuItem.value = 'edit';
+            moreMenuItems.appendChild(moreMenuItem);
+
+            let moreMenuBtn = document.createElement('button');
+            moreMenuBtn.className = 'more-menu-btn';
+            moreMenuBtn.id='edit-' + item.id;
+            moreMenuBtn.innerText = 'Edit';
+            moreMenuBtn.role = 'menuitem';
+            moreMenuBtn.type = 'button';
+            moreMenuBtn.value = 'edit';
+            moreMenuBtn.addEventListener('click', editTask, false);
+            moreMenuItem.appendChild(moreMenuBtn);
+
+            moreMenuBtn = document.createElement('button');
+            moreMenuBtn.className = 'more-menu-btn';
+            moreMenuBtn.id='delete-' + item.id;
+            moreMenuBtn.innerText='Delete';
+            moreMenuBtn.role = 'menuitem';
+            moreMenuBtn.type = 'button';
+            moreMenuBtn.value = 'delete';
+            moreMenuBtn.addEventListener('click', deleteTask, false);
+            moreMenuItem.appendChild(moreMenuBtn);
+
+                    
+
             liElement.appendChild(checkbox);
             liElement.appendChild(title)
             liElement.appendChild(taskActions);
             ulElment.appendChild(liElement);
         });
+
+       
 
         header.innerHTML = category.name;
         singleTask.appendChild(header);
@@ -128,4 +254,9 @@ function viewTasks(){
         singleTask.appendChild(ulElment);
         taskView.appendChild(singleTask);
     });
+
+   
 }
+
+
+
--- public/js/test.js
+++ public/js/test.js
@@ -301,3 +301,171 @@ var taskIncomplete=function(){
 var ajaxRequest=function(){
 	console.log("AJAX Request");
 }
+
+
+
+// The original pure JavaScript, by Andrew Chalkey
+
+var taskInput = document.getElementById("new-task"); //new-task element
+var addButton = document.getElementsByTagName("button")[0]; // first button on the page
+var incompleteTasksHolder = document.getElementById("incomplete-tasks"); // incomplete-tasks
+var completedTasksHolder = document.getElementById("completed-tasks"); // completed-tasks
+
+// New Task List Item
+
+var createNewTaskElement = function(taskString) {
+	// Create list item
+	var listItem = document.createElement("li");
+	// input (checkbox) 
+	var checkBox = document.createElement("input"); // checkbox
+	// label
+	var label = document.createElement("label"); // label
+	// input (text)
+	var editInput = document.createElement("input"); //text
+	// button.edit
+	var editButton = document.createElement("button"); 
+	// button.delete
+	var deleteButton = document.createElement("button");
+			
+	// Each of these elements needs modifying
+	
+	checkBox.type = "checkbox";
+	editInput.type = "text";
+	
+	editButton.innerText = "Edit";
+	editButton.className = "edit";
+	deleteButton.innerText = "Delete";
+	deleteButton.className = "delete";
+	
+	label.innerText = taskString;
+	
+	
+	// Each element needs appending		
+	
+	listItem.appendChild(checkBox);
+	listItem.appendChild(label);
+	listItem.appendChild(editInput);
+	listItem.appendChild(editButton);
+	listItem.appendChild(deleteButton);
+	return listItem;
+}
+
+// Add a new task.
+
+var addTask = function() {
+	console.log("Add task...");
+		// Create a new list item with the text from #new-task:
+	var listItem = createNewTaskElement(taskInput.value);
+	
+	// Append listItem to incompleteTasksHolder
+	incompleteTasksHolder.appendChild(listItem);
+	bindTaskEvents(listItem, taskCompleted);	
+}
+
+var editTask = function() {
+	console.log("Edit task...");
+
+	var listItem = this.parentNode;
+	
+	var editInput = listItem.querySelector("input[type=text]");
+	var label = listItem.querySelector("label");
+	
+	var containsClass = listItem.classList.contains("editMode");
+
+	// if the class of the parent list item is .editMode
+	if(containsClass) {
+				// we want to switch from .editMode
+				// label text become the input's value
+				label.InnerText = editInput.value;
+			} else  {
+				// Switch to .editMode
+				// input value become the label's text
+				
+				editInput.value = label.innerText;
+				}
+		
+		listItem.classList.toggle("editMode");
+		// toggle Edit Mode on the list item 
+		
+}
+
+var deleteTask = function() {
+	console.log("Delete task...");
+	var listItem = this.parentNode;
+	var ul = listItem.parentNode;
+	
+	// Remove the parent list item from our unordered list.
+	
+	ul.removeChild(listItem);	
+}
+
+// Mark a task as complete.
+var taskCompleted = function() {
+	console.log("Task complete...");
+	// Append to the #completed-tasks class 
+	var listItem = this.parentNode;
+	completedTasksHolder.appendChild(listItem);
+	bindTaskEvents(listItem, taskIncomplete);
+			
+}
+
+var taskIncomplete = function() {	
+	console.log("Task incomplete...");		
+	// Append this to #incomplete-tasks.
+	var listItem = this.parentNode;
+	incompleteTasksHolder.appendChild(listItem);
+	bindTaskEvents(listItem, taskCompleted);		
+			
+}
+			
+var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
+	console.log("Bind list item events.");
+	
+	var checkbox = taskListItem.querySelector("input[type=checkbox]");
+	var editButton = taskListItem.querySelector("button.edit");
+	var deleteButton = taskListItem.querySelector("button.delete");
+	
+	// bind editTask to edit button
+	editButton.onclick = editTask;
+	
+	// bind deleteTask to the delete button
+	deleteButton.onclick = deleteTask;
+	
+	checkbox.onchange = checkBoxEventHandler;
+		// bind checkBoxEventHandler to checkbox
+}
+// set the click handler to the addTask function
+addButton.onclick = addTask;
+
+// Cycle over incompleteTaksHolder ul list items
+	for(var i = 0; i < incompleteTasksHolder.children.length; i++) {
+		bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
+}
+	
+	for(var i = 0; i < completedTasksHolder.children.length; i++) {
+		bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
+}
+
+
+
+<div class="more">
+        <button id="more-btn" class="more-btn">
+            <span class="more-dot"></span>
+            <span class="more-dot"></span>
+            <span class="more-dot"></span>
+        </button>
+        <div class="more-menu">
+            <div class="more-menu-caret">
+                <div class="more-menu-caret-outer"></div>
+                <div class="more-menu-caret-inner"></div>
+            </div>
+            <ul class="more-menu-items" tabindex="-1" role="menu" aria-labelledby="more-btn" aria-hidden="true">
+                <li class="more-menu-item" role="presentation">
+                    <button type="button" class="more-menu-btn" role="menuitem">Share</button>
+                </li>
+                <li class="more-menu-item" role="presentation">
+                    <button type="button" class="more-menu-btn" role="menuitem">Copy</button>
+                </li>
+            </ul>
+        </div>
+    </div>
